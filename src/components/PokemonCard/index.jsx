ecb6650b97b20ce8147a75bcdeea0041d590005c
BernardoKrause <bernardokrause@hotmail.com>
2023-02-26 15:10:04 -0300

    initial commit

---
+++ src/components/PokemonCard/index.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function PokemonCard({ name, image, types }) {
+
+  const typeHandler = () => {
+    if (types[1]) {
+      return types[0].type.name + " | " + types[1].type.name;
+    }
+    return types[0].type.name;
+  }
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography gutterBottom variant="p" component="div">
+            {typeHandler(types)}
+          </Typography>
+          {/*<Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+  </Typography>*/}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
